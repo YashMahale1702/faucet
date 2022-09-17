fcbfbbd2f7d6051a7bad7944e8005255a7d143d8
Yash Mahale <yashmahale1702@gmail.com>
2022-09-18 01:56:52 +0530

    UI changes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,105 @@
 import { useEffect } from 'react';
+import { useState } from 'react';
+import detectEthereumProvider from '@metamask/detect-provider';
+
 import './App.scss';
 
+import Web3 from 'web3';
+
 function App() {
+    const [Web3Api, setWeb3Api] = useState({
+        provider: null,
+        web3: null
+    });
+
+    const [Account, setAccount] = useState(null);
+
     useEffect(() => {
         const loadProvider = async () => {
-            // These are already available, directly injected by the Metamask
-            console.log(window.ethereum);
-            console.log(window.web3);
+            // provider is -> Metamask
+            const provider = await detectEthereumProvider();
+
+            if (provider) {
+                setWeb3Api({
+                    provider,
+                    web3: new Web3(provider)
+                });
+            } else {
+                console.error('Please install Metamask');
+            }
         };
 
         loadProvider();
     }, []);
 
+    useEffect(() => {
+        const getAccount = async () => {
+            const accounts = await Web3Api.web3.eth.getAccounts();
+            setAccount(accounts[0]);
+        };
+
+        Web3Api.web3 && getAccount();
+    }, [Web3Api.web3]);
+
     const enableEthHandler = async () => {
-        const accounts = await window.ethereum.request({
+        const account = await Web3Api.provider.request({
             method: 'eth_requestAccounts'
         });
 
-        console.log(accounts);
+        console.log(account);
     };
 
     return (
         <div className='faucet-wrapper'>
             <div className='faucet'>
+                <div className='account'>
+                    <strong>Account: </strong>
+                    <span>
+                        {' '}
+                        {Account ? (
+                            Account
+                        ) : (
+                            <button
+                                className='button is-white'
+                                onClick={enableEthHandler}
+                            >
+                                Connect metamask
+                            </button>
+                        )}
+                    </span>
+                </div>
                 <div className='balance-view'>
                     Current Balance: <strong>10</strong> ETH
                 </div>
-                <button className='btn' onClick={enableEthHandler}>
-                    Enable Ethereum
-                </button>
-                <button className='btn'>Donate</button>
-                <button className='btn'>Withdraw</button>
+                <div className='btn-wrapper'>
+                    <button className='button is-link '>Donate</button>
+                    <button className='button is-primary '>Withdraw</button>
+                </div>
             </div>
         </div>
     );
 }
 
 export default App;
+
+// * Old LoadProvider useEffect
+// let provider = null;
+// if (window.ethereum) {
+//     provider = window.ethereum;
+//     try {
+//         await provider.request({ method: 'eth_requestAccounts' });
+//     } catch {
+//         console.log('User denied accounts access');
+//     }
+// } else if (window.Web3) {
+//     provider = window.web3.currentProvider;
+// } else if (!process.env.production) {
+//     provider = new Web3.providers.HttpProvider(
+//         'http://localhost:7545'
+//     );
+// }
+
+// setWeb3Api({
+//     provider,
+//     web3: new Web3(provider)
+// });
